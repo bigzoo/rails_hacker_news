044e2cd8556ef726dda0d752fbf582f5586f40be
Chris Nyaga <nyagachris75@gmail.com>
2017-08-02 17:06:52 +0300

    Posts controller with CRUD functionality.

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,43 @@
+class PostsController < ApplicationController
+  def new
+    @post = Post.new
+  end
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      flash[:notice]= "Post Created Succesfully!"
+      redirect_to posts_path
+    else
+      render :new
+    end
+  end
+  def index
+    @posts = Post.all
+  end
+  def show
+    @post = Post.find(params[:id])
+  end
+  def edit
+    @post = Post.find(params[:id])
+  end
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      flash[:notice]= "Post Updated Succesfully!"
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
+  end
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    flash[:notice]= "Post Deleted Succesfully!"
+    redirect_to posts_path
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:user_id,:link,:votes,:ranking,:description)
+  end
+end
